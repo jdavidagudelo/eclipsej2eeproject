c50c7cbcca0d304c32c1c7b69d012e5e0b3d6b3c
jdavidagudelo <jdaaa2009@gmail.com>
2015-02-07 17:33:15 -0500

    Including JSF and managed Beans

---
+++ ProjectEAR/web/src/com/artica/example/ConceptController.java
@@ -0,0 +1,13 @@
+package com.artica.example;
+
+import javax.enterprise.context.SessionScoped;
+import org.apache.myfaces.webapp.StartupServletContextListener;
+import javax.inject.Named;
+@Named("conceptController")
+@SessionScoped
+public class ConceptController {
+
+	public ConceptController() {
+	}
+
+}
